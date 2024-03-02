9077aab9480fddafa6be15ecb85d802ce03b5413
Anton <150729033+biorge@users.noreply.github.com>
2024-03-02 14:57:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    	while (b != a)
-	{
-		if (a > b)
-		{
-			a = a - b;
-		}
-		else
-		{
-			b = b - a;
-		}
-	}
-
-	return a;
+  while (b != a) {
+    if (a > b)
+      {
+        a = a - b;
+      }
+      else {
+        b = b - a;
+      }
+  }
+  return a;
 }
